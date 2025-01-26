cf6aafa6c08e54db2f05c7d6e6201be683cb24dc
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-01-26 18:15:06 +0000

    Add infra
    [ghstack-poisoned]

---
+++ infra/variable.tf
@@ -0,0 +1,43 @@
+variable "account" {
+  type        = string
+  description = "Cloudflare account ID"
+  default     = "d4e789904d6943d8cd524e19c5cb36bd"
+}
+
+variable "zone" {
+  type        = string
+  description = "Cloudflare zone ID"
+  default     = "6c9b58fc27fe154e5ac9addbc872fa9d"
+}
+
+variable "projects" {
+  type        = map(string)
+  description = "Map of Cloudflare Pages projects"
+  default = {
+    shikanime-studio = "shikanime-studio"
+    reiya = "reiya"
+    links = "links"
+  }
+}
+
+variable "dns_records" {
+  type = map(object({
+    name    = string
+    project = string
+  }))
+  description = "Map of DNS records"
+  default = {
+    shikanime-studio = {
+        name = "shikanime.studio"
+        project = "shikanime-studio"
+    }
+    reiya = {
+      name    = "reiya.shikanime.studio"
+      project = "reiya"
+    }
+    links = {
+      name    = "links.shikanime.studio"
+      project = "links"
+    }
+  }
+}
