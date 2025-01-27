af84c57770dc0b807e005f8b3aa7eac93185311e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-01-27 23:55:44 +0000

    Fix broken React DOM
    [ghstack-poisoned]

--- packages/reiya/astro.config.mjs
+++ packages/reiya/astro.config.mjs
@@ -1,6 +1,10 @@
 import cloudflare from "@astrojs/cloudflare";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
+import process from "node:process";
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,5 +24,14 @@ export default defineConfig({
       enabled: true,
     },
   }),
-  site: "https://reiya.shikanime.studio",
+  site: process.env.SITE || "https://reiya.shikanime.studio",
+  vite: {
+    resolve: {
+      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+      alias: import.meta.env.PROD && {
+        "react-dom/server": "react-dom/server.edge",
+      },
+    },
+  },
 });
