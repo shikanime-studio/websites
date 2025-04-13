501dbda7ce28f07b1894e78efabd8d60a0501e32
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-04-13 20:59:17 +0200

    Add persona specific links
    ghstack-source-id: 3b4b4ec0b6d67d72968e850a802b08c5f3aaccc9
    Pull Request resolved: https://github.com/shikanime/websites/pull/79

--- infra/variable.tf
+++ infra/variable.tf
@@ -40,5 +40,7 @@ variable "dns_records" {
 variable "d1s" {
   type        = map(string)
   description = "Map of Cloudflare D1 databases"
-  default = {}
+  default = {
+    links = "links"
+  }
 }
