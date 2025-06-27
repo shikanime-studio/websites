4d69e633d476a05d8f48c8c4ad258eea20064bac
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-06-28 00:19:55 +0900

    Merge Nix configs
    ghstack-source-id: c763d4b5ec72ed228e2569f2feeb5e4b2037c1e6
    Pull Request resolved: https://github.com/shikanime-studio/websites/pull/81

--- flake.nix
+++ flake.nix
@@ -28,8 +28,88 @@
       imports = [
         devenv.flakeModule
         treefmt-nix.flakeModule
-        ./nix/devenv.nix
       ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            enableDefaultExcludes = true;
+            programs = {
+              actionlint.enable = true;
+              hclfmt.enable = true;
+              nixfmt.enable = true;
+              prettier = {
+                enable = true;
+                includes = [
+                  "*.astro"
+                  "*.js"
+                  "*.json"
+                  "*.jsx"
+                  "*.md"
+                  "*.mjs"
+                  "*.ts"
+                  "*.tsx"
+                  "*.webmanifest"
+                  "*.xml"
+                  "*.yaml"
+                ];
+                settings.plugins = [
+                  "@prettier/plugin-xml"
+                  "@trivago/prettier-plugin-sort-imports"
+                  "prettier-plugin-astro"
+                  "prettier-plugin-tailwindcss"
+                ];
+              };
+              shfmt.enable = true;
+              sqlfluff = {
+                enable = true;
+                dialect = "sqlite";
+              };
+              statix.enable = true;
+              taplo.enable = true;
+              terraform.enable = true;
+            };
+            settings.global.excludes = [
+              "*.gif"
+              "*.ico"
+              "*.jpg"
+              "*.png"
+              "*.svg"
+              "*.txt"
+              "*.webp"
+              "**/node_modules"
+            ];
+          };
+          devenv.shells.default = {
+            containers = pkgs.lib.mkForce { };
+            languages = {
+              opentofu.enable = true;
+              nix.enable = true;
+              javascript = {
+                enable = true;
+                npm = {
+                  enable = true;
+                  install.enable = true;
+                };
+              };
+            };
+            cachix = {
+              enable = true;
+              push = "shikanime";
+            };
+            git-hooks.hooks = {
+              deadnix.enable = true;
+              denolint.enable = true;
+              flake-checker.enable = true;
+              shellcheck.enable = true;
+              tflint.enable = true;
+            };
+            packages = [
+              pkgs.gitnr
+            ];
+          };
+        };
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
