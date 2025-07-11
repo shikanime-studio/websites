6e5bedb39b183f0985e9a284f1cc09c445e39fb3
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-07-11 19:00:46 +0900

    None on "Add new Reiya project"
    [ghstack-poisoned]

--- packages/reiya/src/lib/items.ts
+++ packages/reiya/src/lib/items.ts
@@ -1,10 +1,6 @@
 import { items } from "../schema";
 import { DrizzleD1Database } from "drizzle-orm/d1";
 
-export function getItems(
-  db: DrizzleD1Database,
-) {
-  return db
-    .select()
-    .from(items);
+export function getItems(db: DrizzleD1Database) {
+  return db.select().from(items);
 }
