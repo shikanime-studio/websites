09119538324dd23e19650b014dc601fd01c78e16
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-22 00:27:09 +0900

    Update base for None on "Add secretspec"
    [ghstack-poisoned]

---
+++ packages/baguetto/migrations/0002_magical_timeslip.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `accounts` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`user_id` integer NOT NULL,
+	`provider` text NOT NULL,
+	`subject` text NOT NULL,
+	`access_token` text,
+	`expires_at` integer,
+	`refresh_token` text,
+	`scope` text,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `user_to_accounts` (
+	`user_id` integer NOT NULL,
+	`account_id` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`account_id`) REFERENCES `accounts`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);--> statement-breakpoint
+ALTER TABLE `users` DROP COLUMN `google_id`;
