a8e56923844f08a4878d2c54186b28c9e8d8b22f
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-08-21 20:55:37 +0900

    Add new Reiya project
    ghstack-source-id: ab842dd20b9d047306584943cebe52502ec67dc3
    Pull Request resolved: https://github.com/shikanime-studio/websites/pull/82

--- infra/variable.tf
+++ infra/variable.tf
@@ -15,6 +15,7 @@ variable "projects" {
   description = "Map of Cloudflare Pages projects"
   default = {
     links = "links"
+    baguetto = "baguetto"
     www   = "www"
   }
 }
@@ -30,6 +31,10 @@ variable "dns_records" {
       name    = "links.shikanime.studio"
       project = "links"
     }
+    baguetto = {
+      name    = "baguetto.shikanime.studio"
+      project = "baguetto"
+    }
     www = {
       name    = "shikanime.studio"
       project = "www"
@@ -40,5 +45,7 @@ variable "dns_records" {
 variable "d1s" {
   type        = map(string)
   description = "Map of Cloudflare D1 databases"
-  default     = {}
+  default = {
+    baguetto = "baguetto"
+  }
 }
---
+++ legacy/account/page.tsx
@@ -0,0 +1,9 @@
+import Wip from "../components/Wip";
+
+export default async function Account() {
+  return (
+    <>
+      <Wip />
+    </>
+  );
+}
