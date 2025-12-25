2b30b55effef60e5695f406761a4d6ca6083095c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2025-12-25 23:51:14 +0100

    Remove logger dev config

--- packages/reiya/src/lib/db.ts
+++ packages/reiya/src/lib/db.ts
@@ -2,7 +2,5 @@ import type { Schema } from "../schema";
 import { drizzle } from "drizzle-orm/d1";
 
 export function createD1Database(locals: App.Locals) {
-  return drizzle<Schema>(locals.runtime.env.DB, {
-    logger: locals.runtime.env.DEV,
-  });
+  return drizzle<Schema>(locals.runtime.env.DB);
 }
