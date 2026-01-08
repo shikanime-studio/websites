d9e43b924fbeec914145b0daf6927352449b817e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-08 22:55:54 +0100

    Update (base update)
    [ghstack-poisoned]

--- apps/fade/src/vite-env.d.ts
+++ apps/fade/src/vite-env.d.ts
@@ -1,20 +1,2 @@
 /// <reference types="vite/client" />
 
-// File System Access API types
-interface FileSystemDirectoryHandle {
-  kind: "directory";
-  name: string;
-  values(): AsyncIterableIterator<FileSystemHandle>;
-}
-
-interface FileSystemFileHandle {
-  kind: "file";
-  name: string;
-  getFile(): Promise<File>;
-}
-
-type FileSystemHandle = FileSystemDirectoryHandle | FileSystemFileHandle;
-
-interface Window {
-  showDirectoryPicker(): Promise<FileSystemDirectoryHandle>;
-}
