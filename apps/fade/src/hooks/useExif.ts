1284f9b0804e01b53d509937a3cfd5eaac292f71
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-11 23:16:29 +0100

    Update (base update)
    [ghstack-poisoned]

--- apps/fade/src/hooks/useExif.ts
+++ apps/fade/src/hooks/useExif.ts
@@ -1,14 +1,15 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { getExifTags } from "../lib/exif";
+import type { FileItem } from "../lib/fs";
 
-export function useExif(file: File | null) {
+export function useExif(fileItem: FileItem | null) {
   const { data } = useSuspenseQuery({
-    queryKey: ["exif", file?.name, file?.lastModified, file?.size],
+    queryKey: ["exif", fileItem?.handle.name],
     queryFn: async () => {
-      if (!file) return null;
+      if (!fileItem) return null;
 
       try {
-        return await getExifTags(file);
+        return await getExifTags(fileItem);
       } catch (err) {
         console.error("Failed to parse EXIF:", err);
         return null;
