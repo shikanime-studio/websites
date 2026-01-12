05e24a37122b6c995294390bca108151458ac216
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-12 16:10:57 +0100

    Update
    [ghstack-poisoned]

--- apps/fade/src/env.d.ts
+++ apps/fade/src/env.d.ts
@@ -1,11 +1,6 @@
 /// <reference types="@webgpu/types" />
 /// <reference types="vite/client" />
 
-declare module "*.css?url" {
-  const content: string;
-  export default content;
-}
-
 interface ImportMetaEnv {
   readonly VITE_MIXPANEL_TOKEN: string;
   readonly VITE_MIXPANEL_API_HOST: string;
