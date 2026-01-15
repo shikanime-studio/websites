4fe499ca9aa3eceeeff33af334ab0f7460a01fc3
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-15 01:23:21 +0100

    Merge reset mechanism in lighting hook
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: I976165bbf37471578013c6d03e216a3b6a6a6964
    ghstack-source-id: c8f8ea559c23af9d78fbbd30193cc84d04207438
    Pull-Request: https://github.com/shikanime-studio/websites/pull/193

--- apps/fade/src/hooks/useLighting.ts
+++ apps/fade/src/hooks/useLighting.ts
@@ -3,27 +3,37 @@ import { createContext, useContext } from "react";
 export interface LightingContextType {
   exposure: number;
   setExposure: (value: number) => void;
+  resetExposure: () => void;
   contrast: number;
   setContrast: (value: number) => void;
+  resetContrast: () => void;
   saturation: number;
   setSaturation: (value: number) => void;
+  resetSaturation: () => void;
   highlights: number;
   setHighlights: (value: number) => void;
+  resetHighlights: () => void;
   shadows: number;
   setShadows: (value: number) => void;
+  resetShadows: () => void;
   whites: number;
   setWhites: (value: number) => void;
+  resetWhites: () => void;
   blacks: number;
   setBlacks: (value: number) => void;
+  resetBlacks: () => void;
   tint: number;
   setTint: (value: number) => void;
+  resetTint: () => void;
   temperature: number;
   setTemperature: (value: number) => void;
+  resetTemperature: () => void;
   vibrance: number;
   setVibrance: (value: number) => void;
+  resetVibrance: () => void;
   hue: number;
   setHue: (value: number) => void;
-  reset: () => void;
+  resetHue: () => void;
 }
 
 export const LightingContext = createContext<LightingContextType | null>(null);
