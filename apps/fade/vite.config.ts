12fceb110c24fdb9ba75e07d51703bde12830d68
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-31 22:17:48 +0100

    Update (base update)
    [ghstack-poisoned]

--- apps/fade/vite.config.ts
+++ apps/fade/vite.config.ts
@@ -4,16 +4,11 @@ import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
     devtools(),
     cloudflare({ viteEnvironment: { name: "ssr" } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
