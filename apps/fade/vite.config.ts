c1b6e4a22d65f4a831d83e9a3dfc82bd36c0ee02
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-02-19 19:40:03 +0100

    Update
    [ghstack-poisoned]

--- apps/fade/vite.config.ts
+++ apps/fade/vite.config.ts
@@ -1,23 +1,24 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ["./tsconfig.json"],
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tsconfigPaths({
+      projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
     viteReact(),
   ],
-});
+})
 
-export default config;
+export default config
