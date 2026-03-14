c905f50768a6f5764ec26254ce46e6e0c8fe2558
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-03-14 17:36:04 +0100

    Migrate deprecated vitest configs
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: Ie2da4615e23b5544954533aa7e88666f6a6a6964
    ghstack-source-id: 87c10ef0057abbdf3139eb5facba72e0eb104b1a
    Pull-Request: https://github.com/shikanime-studio/websites/pull/268

--- apps/fade/vite.config.ts
+++ apps/fade/vite.config.ts
@@ -2,15 +2,15 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
     devtools(),
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    react(),
     tailwindcss(),
     tanstackStart(),
-    viteReact(),
   ],
 })
